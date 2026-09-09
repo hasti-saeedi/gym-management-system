f9a9a80876a456751769ea61a25cb0b797060ece
Hasti Saeedi <hastisaeedi2005@gmail.com>
2026-09-10 02:05:05 +0430

    add next time file and change of gym model (name of the gym must be uniqe)

--- gyms/models.py
+++ gyms/models.py
@@ -8,6 +8,7 @@ from accounts.models import CustomUser
 class Gym(models.Model):
     name = models.CharField(
         max_length=20,
+        unique=True,
     )
 
     address = models.TextField()
